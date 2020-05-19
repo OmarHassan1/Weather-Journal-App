b3890e5f91c163967a29566b992579806671c591
OmarHassin <boky555555@gmail.com>
2020-05-19 20:41:10 +0200

    Project three

---
+++ README.md
@@ -0,0 +1,12 @@
+# Weather-Journal App Project
+
+This my third project for Front-end
+The project is about to create an asynchronous web app 
+
+
+## How to start
+
+You need to have Node.js and npm locally installed. You can check that by running `node -v && npm -v` in terminal. This should result in two versions shown in terminal.
+
+Then, you have to run `npm start` in order to run the express server locally.
+
---
+++ server.js
@@ -0,0 +1,36 @@
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(express.static('website'));
+
+// Setup Server
+ const port = 8080;
+ const server = app.listen(port, ()=>{
+     console.log('server is running :)')
+    console.log(`running on localhost:${port}`);
+
+ });
+
+
+app.get('/weatherData', function(req, res) {
+    res.send(projectData);
+})
+
+app.post('/addWeather', function (req, res) {
+    projectData = req.body;
+    console.log('post request: received');
+    console.log(projectData);
+    res.send('Post received');
+});
