1658d3aeb609036b44d4049875214086ddddb9b6
OmarHassin <boky555555@gmail.com>
2020-12-24 21:47:29 +0200

    update

--- server.js
+++ server.js
@@ -2,35 +2,37 @@
 const projectData = {};
 
 // Require Express to run server and routes
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const serverless = require("serverless-http");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 // Setup Server
- const port = 8080;
- const server = app.listen(port, ()=>{
-     console.log('server is running :)')
-    console.log(`running on localhost:${port}`);
+const port = 8000;
 
- });
-
-
-app.get('/weatherData', function(req, res) {
-    res.send(projectData);
-})
-
-app.post('/addWeather', function (req, res) {
-    projectData = req.body;
-    console.log('post request: received');
-    console.log(projectData);
-    res.send('Post received');
+const server = app.listen(port, () => {
+  console.log("server is running :)");
+  console.log(`running on localhost:${port}`);
 });
+
+app.get("/weatherData", function (req, res) {
+  res.send(projectData);
+});
+
+app.post("/addWeather", function (req, res) {
+  projectData = req.body;
+  console.log("post request: received");
+  console.log(projectData);
+  res.send("Post received");
+});
+
+module.exports.handler = serverless(app);
